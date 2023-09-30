1b11ccf35fd2bc6622794d3a6916471c0f844857
JMVAjaltoun <145804968+JMVAjaltoun@users.noreply.github.com>
2023-09-30 22:15:55 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+function loadComponent(url, containerId) {
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById(containerId).innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Error loading component:', error);
+      });
+  }
